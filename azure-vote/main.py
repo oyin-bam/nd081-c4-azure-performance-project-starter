9ea8473d464b3290e4ef54b9d070d144191fbb30
oyin <oyindaakinyemi@gmail.com>
2022-07-30 18:39:45 +0100

    changed trace message

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -78,12 +78,12 @@ def index():
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
         with tracer.span(name='CATVOTES'):
-            logger.info("Cat Votes", extra=vote1)
+            logger.info("CATVOTES", vote1)
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
         with tracer.span(name='DOGVOTES'):
-            logger.info("Dog votes", extra=vote2)
+            logger.info("DOGVOTES", vote2)
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
